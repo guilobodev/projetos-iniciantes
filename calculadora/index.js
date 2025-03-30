afb23d305a6915a1ce4c3d00d9e869900cdd4dbf
guilobodev <guilhermelobobrayner@gmail.com>
2025-03-30 13:34:49 -0300

    logica da calculadora

--- calculadora/index.js
+++ calculadora/index.js
@@ -3,55 +3,12 @@
 const display = document.querySelector("#display");
 const numeros = document.querySelectorAll("[id*= tecla");
 const operadores = document.querySelectorAll("[id*=operador]");
-const limparDisplay = document.querySelector("#limparDisplay");
 
 let novoNumero = true;
 let numeroAnterior;
 let operador;
-let numeroAtual = display.textContent;
-let total;
 
-function atualizarDisplay(texto) {
-  if (novoNumero) {
-    display.textContent = texto;
-    novoNumero = false;
-  } else {
-    display.textContent += texto;
-    //
-  }
-}
 
-limparDisplay.addEventListener("click", (limpar) => {
-  display.textContent = "";
-  novoNumero = true
-});
-
-function inserirNumero(event) {
-  atualizarDisplay(event.target.textContent);
-}
-
-const operacaoPendente = () => operador !== undefined;
-
-function calcular() {
-  if (operacaoPendente()) {
-    let numeroAtual = parseFloat(display.textContent);
-    novoNumero = true;
-    if (operador == "+") {
-      atualizarDisplay(numeroAnterior + numeroAtual);
-    }
-  }
-}
-
-function selecionarOperador(event) {
-  if (!novoNumero) {
-    calcular();
-    novoNumero = true;
-    operador = event.target.textContent;
-    numeroAnterior = parseFloat(display.textContent);
-    console.log(operador);
-    console.log(numeroAnterior);
-  }
-}
 //numeros
 numeros.forEach((numero) => {
   numero.addEventListener("click", inserirNumero);
@@ -61,3 +18,148 @@ numeros.forEach((numero) => {
 operadores.forEach((operador) => {
   operador.addEventListener("click", selecionarOperador);
 });
+
+function atualizarDisplay(texto) {
+  if (novoNumero) {
+    display.textContent = texto.toLocaleString('BR');
+    novoNumero = false;
+  } else {
+    display.textContent += texto.toLocaleString('BR');
+    //
+  }
+}
+
+function inserirNumero(event) {
+  console.log(event);
+  atualizarDisplay(event.target.textContent);
+}
+
+const operacaoPendente = () => operador !== undefined;
+
+function calcular() {
+  if (operacaoPendente()) {
+    let numeroAtual = parseFloat(display.textContent.replace(',','.'));
+    novoNumero = true;
+    if (operador == "+") {
+      atualizarDisplay(numeroAnterior + numeroAtual);
+    } else if (operador == "-") {
+      atualizarDisplay(numeroAnterior - numeroAtual);
+    } else if (operador == "/") {
+      atualizarDisplay(numeroAnterior / numeroAtual);
+    } else if (operador == "*") {
+      atualizarDisplay(numeroAnterior * numeroAtual);
+    }
+  }
+}
+
+function selecionarOperador(event) {
+  if (!novoNumero) {
+    calcular();
+    novoNumero = true;
+    operador = event.target.textContent;
+    numeroAnterior = parseFloat(display.textContent.replace(',','.'));
+    console.log(operador);
+    console.log(numeroAnterior);
+  }
+}
+
+const limparDisplay = () => {
+  display.textContent = "";
+};
+
+document
+  .querySelector("#limparDisplay")
+  .addEventListener("click", limparDisplay);
+
+document.querySelector("#igual").addEventListener("click", ativarIgual);
+
+function ativarIgual() {
+  calcular();
+  operador = undefined;
+}
+
+document
+  .querySelector("#limparCalculo")
+  .addEventListener("click", limparCalculo);
+
+function limparCalculo() {
+  limparDisplay();
+  operador = undefined;
+  novoNumero = true;
+  numeroAnterior = undefined;
+}
+
+function removerUltimoNumero() {
+  display.textContent = display.textContent.slice(0, -1);
+}
+
+document
+  .querySelector("#backspace")
+  .addEventListener("click", removerUltimoNumero);
+
+function inverterSinal() {
+  novoNumero = true;
+  atualizarDisplay(display.textContent * -1);
+}
+
+document.querySelector("#inverter").addEventListener("click", inverterSinal);
+
+function existeDecimal() {
+  display.textContent.indexOf(',') !== -1;
+}
+function existeValor () {
+  display.textContent.length > 0
+}
+
+function inserirDecimal() {
+  if (!existeDecimal()) {
+    if (existeValor()) {
+      atualizarDisplay("0,");
+    } else {
+      atualizarDisplay(",");
+    }
+  }
+}
+
+document.querySelector("#decimal").addEventListener("click", inserirDecimal);
+
+
+const mapaTeclado = {
+  0: 'tecla0',
+  1: 'tecla1',
+  2: 'tecla2',
+  3: 'tecla3',
+  4: 'tecla4',
+  5: 'tecla5',
+  6: 'tecla6',
+  7: 'tecla7',
+  8: 'tecla8',
+  9: 'tecla9',
+  '/': 'operadorDividir',
+  '*': 'operadorMultiplicar',
+  '-': 'operadorSubtrair',
+  '+': 'operadorAdicionar',
+  '=': 'igual',
+  Enter: 'igual',
+  Backspace: 'backspace',
+  c: 'limparDisplay',
+  Escape: 'limparCalculo',
+  ',': 'decimal',
+};
+
+
+function mapearTeclado (event) {
+    const tecla = event.key;
+
+    const teclaPermitida = () => {
+      Object.keys(mapaTeclado).indexOf(tecla) !== -1
+    }
+
+    if (teclaPermitida()) {
+      document.querySelector(`#${mapaTeclado[tecla]}`).click();
+    }
+
+    
+    
+}
+document.addEventListener('keydown',mapearTeclado)
